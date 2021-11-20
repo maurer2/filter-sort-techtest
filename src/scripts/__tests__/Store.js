a6c6e8d60f14e50b7f4a4ed241a2dbd39d304125
Martin <mr.mmaurer+work@gmail.com>
2021-11-20 22:50:38 +0000

    Added product filter only tests

--- src/scripts/__tests__/Store.js
+++ src/scripts/__tests__/Store.js
@@ -32,4 +32,97 @@ describe("Filtering", () => {
       expect(store.hasProviderFilters).toEqual(false);
     });
   });
+
+  describe("GIVEN that broadband product filter is enabled and provider filters are disabled", () => {
+    beforeEach(() => {
+      store.setProductFilter('broadband');
+
+      result = store.deals;
+    });
+
+    it("THEN should return 4 deals", () => {
+      expect(result.length).toBe(4);
+    });
+
+    it("THEN should only contain broadband deals", () => {
+      const productTypesBag = result.flatMap((resultEntry) => resultEntry.productTypes);
+      const productTypes = [...new Set(productTypesBag)];
+      const targetProductTypes = [
+        'Broadband',
+        'Phone',
+        'Fibre Broadband'
+      ];
+
+      const hasEveryTargetType = productTypes.every((productTyp) => targetProductTypes.includes(productTyp));
+      expect(hasEveryTargetType).toBe(true);
+
+      const hasOtherProductType = productTypes.find((productTyp) => !targetProductTypes.includes(productTyp));
+      expect(hasOtherProductType).toBeFalsy();
+    });
+  });
+
+  describe("GIVEN that broadband and tv product filters are enabled and provider filters are disabled", () => {
+    beforeEach(() => {
+      store.setProductFilter('broadband');
+      store.setProductFilter('tv');
+
+      result = store.deals;
+    });
+
+    it("THEN should return 4 deals", () => {
+      expect(result.length).toBe(4);
+    });
+
+    it("THEN should only contain broadband and tv deals", () => {
+      const productTypesBag = result.flatMap((resultEntry) => resultEntry.productTypes);
+      const productTypes = [...new Set(productTypesBag)];
+      const targetProductTypes = [
+        'Broadband',
+        'Phone',
+        'Fibre Broadband',
+        'TV'
+      ];
+
+      const hasEveryTargetType = productTypes.every((productTyp) => targetProductTypes.includes(productTyp));
+      expect(hasEveryTargetType).toBe(true);
+
+      const hasOtherProductType = productTypes.find((productTyp) => !targetProductTypes.includes(productTyp));
+      expect(hasOtherProductType).toBeFalsy();
+    });
+  });
+
+  describe("GIVEN that broadband and mobile product filters are enabled and provider filters are disabled", () => {
+    beforeEach(() => {
+      store.setProductFilter('broadband');
+      store.setProductFilter('mobile');
+
+      result = store.deals;
+    });
+
+    it("THEN should return 1 deal", () => {
+      expect(result.length).toBe(1);
+    });
+
+    it("THEN should only contain broadband and mobile deals", () => {
+      const productTypesBag = result.flatMap((resultEntry) => resultEntry.productTypes);
+      const productTypes = [...new Set(productTypesBag)];
+      const targetProductTypes = [
+        'Broadband',
+        'Phone',
+        'Fibre Broadband',
+        'Mobile'
+      ];
+
+      const hasEveryTargetType = productTypes.every((productTyp) => targetProductTypes.includes(productTyp));
+      expect(hasEveryTargetType).toBe(true);
+
+      const hasOtherProductType = productTypes.find((productTyp) => !targetProductTypes.includes(productTyp));
+      expect(hasOtherProductType).toBeFalsy();
+    });
+  });
 });
+
+/*
+ * WHEN filtering by Sky THEN show the 1 deal for Sky only
+ * WHEN filtering by BT, broadband AND tv THEN show the 2 deals for BT with broadband and tv only
+ */
