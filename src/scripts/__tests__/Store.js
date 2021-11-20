9a3c7fe4985e9dbeeaa7674d89ddcc650d7081a9
Martin <mr.mmaurer+work@gmail.com>
2021-11-20 23:33:51 +0000

    Added remaining unit tests

--- src/scripts/__tests__/Store.js
+++ src/scripts/__tests__/Store.js
@@ -1,6 +1,13 @@
 import Store from "../Store";
 import mockData from "../../../public/db.json";
 
+const getProviderIdByName = (providerName) => {
+  const providers = mockData.deals.map((deal) => deal.provider);
+  const entry = providers.find((provider) => provider.name === providerName);
+
+  return entry.id;
+};
+
 let result;
 let store;
 
@@ -120,9 +127,68 @@ describe("Filtering", () => {
       expect(hasOtherProductType).toBeFalsy();
     });
   });
-});
 
-/*
- * WHEN filtering by Sky THEN show the 1 deal for Sky only
- * WHEN filtering by BT, broadband AND tv THEN show the 2 deals for BT with broadband and tv only
- */
+  describe("GIVEN that sky provider filter is enabled and product filters are disabled", () => {
+    beforeEach(() => {
+      const idSky = getProviderIdByName('Sky');
+      store.setProviderFilter(idSky);
+
+      result = store.deals;
+    });
+
+    it("THEN should return 1 deal", () => {
+      expect(result.length).toBe(1);
+    });
+
+    it("THEN should only contain sky deal", () => {
+      const providerTypesBag = result.map((resultEntry) => resultEntry.provider.id);
+      const providerTypes = [...new Set(providerTypesBag)];
+      const targetProviderTypes = [getProviderIdByName('Sky')];
+
+      const hasEveryTargetType = providerTypes.every((providerType) => targetProviderTypes.includes(providerType));
+      expect(hasEveryTargetType).toBe(true);
+    });
+  });
+
+  describe("GIVEN that broadband and tv product filters are enabled and provider filter is set to BT", () => {
+    beforeEach(() => {
+      const idBT = getProviderIdByName('BT');
+      store.setProviderFilter(idBT);
+
+      store.setProductFilter('broadband');
+      store.setProductFilter('tv');
+
+      result = store.deals;
+    });
+
+    it("THEN should return 2 deals", () => {
+      expect(result.length).toBe(2);
+    });
+
+    it("THEN should only contain broadband and mobile and BT deals", () => {
+      const productTypesBag = result.flatMap((resultEntry) => resultEntry.productTypes);
+      const productTypes = [...new Set(productTypesBag)];
+      const targetProductTypes = [
+        'Broadband',
+        'Phone',
+        'Fibre Broadband',
+        'TV'
+      ];
+
+      const hasEveryTargetType = productTypes.every((productTyp) => targetProductTypes.includes(productTyp));
+      expect(hasEveryTargetType).toBe(true);
+
+      const hasOtherProductType = productTypes.find((productTyp) => !targetProductTypes.includes(productTyp));
+      expect(hasOtherProductType).toBeFalsy();
+    });
+
+    it("THEN should contain BT deals", () => {
+      const providerTypesBag = result.map((resultEntry) => resultEntry.provider.id);
+      const providerTypes = [...new Set(providerTypesBag)];
+      const targetProviderTypes = [getProviderIdByName('BT')];
+
+      const hasEveryTargetType = providerTypes.every((providerType) => targetProviderTypes.includes(providerType));
+      expect(hasEveryTargetType).toBe(true);
+    });
+  });
+});
